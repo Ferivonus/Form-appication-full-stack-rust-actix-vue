f4a9317f7dc182afe2b75ae9322c8cfcdb8cc2f6
Ferivonus <ferivonus@hotmail.com>
2023-11-11 19:12:09 +0300

    I just wanna kill myself

--- backend-actix-RUST/src/main.rs
+++ backend-actix-RUST/src/main.rs
@@ -5,37 +5,7 @@ use serde_json::json;
 
 mod links;
 
-pub async fn book_page(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
-    let data = json!({
-        "project_name": "Book Store",
-        "books": [
-            {
-                "name": "Harry Potter",
-                "author": "J K Rowling",
-                "image_path": "/static/image/download.jpeg",
-            },
-            {
-                "name": "Lord of the Rings",
-                "author": "Tolkien",
-                "image_path": "/static/image/lord_of.jpeg",
-            },
-            {
-                "name": "Americanah",
-                "author": "Chimamanda Ngozi Adichie",
-                "image_path": "/static/image/americanah.jpeg",
-            },
-            {
-                "name": "Elon Musk",
-                "author": "Unknown",
-                "image_path": "/static/image/elon.jpeg",
-            },
-        ],
-    });
 
-    let template_name = "bookpage_template"; // Name of your template file without the extension
-    let body = hb.render(template_name, &data).unwrap();
-    HttpResponse::Ok().content_type("text/html").body(body)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
