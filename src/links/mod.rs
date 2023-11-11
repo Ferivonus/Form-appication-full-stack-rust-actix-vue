9b7f45749108ff5858d6684005ca58a2e2efee80
Ferivonus <ferivonus@hotmail.com>
2023-11-11 06:33:28 +0300

    I just wanna kill myself

--- src/links/mod.rs
+++ src/links/mod.rs
@@ -3,6 +3,9 @@
 use actix_web::{web, HttpResponse, HttpRequest};
 use rand::Rng;
 use serde::{Serialize, ser::{Serializer, SerializeStruct}};
+use handlebars::Handlebars;
+use serde_json::json;
+
 // Import Serializer for custom serialization
 
 pub fn scoped_config(cfg: &mut web::ServiceConfig) {
@@ -49,4 +52,35 @@ pub async fn greet_user(req: HttpRequest) -> HttpResponse {
     };
 
     HttpResponse::Ok().json(greeting)
+}
+pub async fn book_page(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Book Store",
+        "books": [
+            {
+                "name": "Harry Potter",
+                "author": "J K Rowling",
+                "image_path": "/static/image/download.jpeg",
+            },
+            {
+                "name": "Lord of the Rings",
+                "author": "Tolkien",
+                "image_path": "/static/image/lord_of.jpeg",
+            },
+            {
+                "name": "Americanah",
+                "author": "Chimamanda Ngozi Adichie",
+                "image_path": "/static/image/americanah.jpeg",
+            },
+            {
+                "name": "Elon Musk",
+                "author": "Unknown",
+                "image_path": "/static/image/elon.jpeg",
+            },
+        ],
+    });
+
+    let template_name = "bookpage_template"; // Name of your template file without the extension
+    let body = hb.render(template_name, &data).unwrap();
+    HttpResponse::Ok().content_type("text/html").body(body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
-// src/main.rs
 use actix_files::NamedFile;
-use actix_web::{web, App, HttpServer, HttpRequest};
-use links::greet_user; // Import the greet_user function from links module
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse};
+use handlebars::Handlebars;
+use serde_json::json;
 
 mod links;
 
+pub async fn book_page(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Book Store",
+        "books": [
+            {
+                "name": "Harry Potter",
+                "author": "J K Rowling",
+                "image_path": "/static/image/download.jpeg",
+            },
+            {
+                "name": "Lord of the Rings",
+                "author": "Tolkien",
+                "image_path": "/static/image/lord_of.jpeg",
+            },
+            {
+                "name": "Americanah",
+                "author": "Chimamanda Ngozi Adichie",
+                "image_path": "/static/image/americanah.jpeg",
+            },
+            {
+                "name": "Elon Musk",
+                "author": "Unknown",
+                "image_path": "/static/image/elon.jpeg",
+            },
+        ],
+    });
+
+    let template_name = "bookpage_template"; // Name of your template file without the extension
+    let body = hb.render(template_name, &data).unwrap();
+    HttpResponse::Ok().content_type("text/html").body(body)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_file("bookpage_template", "static/templates/bookpage_template.html")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(handlebars_ref.clone())
             .configure(links::config)
-            .service(web::scope("/api").configure(links::scoped_config))
-            .route("/", web::get().to(|| async { actix_web::HttpResponse::Ok().body("Home Page") }))
-            .route("/hello/{name}", web::get().to(greet_user)) // Use the greet_user function
+            .route("/", web::get().to(|| async { HttpResponse::Ok().body("Home Page") }))
+            .route("/hello/{name}", web::get().to(links::greet_user))
+            .route("/bookpage", web::get().to(links::book_page))
             .service(
                 web::resource("/index.html")
                     .route(web::get().to(|_req: HttpRequest| async {
@@ -22,13 +61,15 @@ async fn main() -> std::io::Result<()> {
                             Err(err) => Err(actix_web::error::ErrorInternalServerError(err)),
                         }
                     }),
-                )
-            )
+                ))
             .service(
                 actix_files::Files::new("/static", "static").show_files_listing()
             )
+            .service(web::scope("/api").configure(links::scoped_config))
     })
     .bind("127.0.0.1:8080")?
+    // .bind("127.0.0.1:80")? masal için tor (These lines are commented out)
+    // .bind("127.0.0.1:8080")? BİLGİSAYAR için tor (These lines are commented out)
     .run()
     .await
 }
